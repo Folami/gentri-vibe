9e783fa169109e52359e3e8e9e0d96826769cba3
Folami <folami.bidmus@gmail.com>
2026-01-30 21:32:46 +0100

    Navbar logo Size edit

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,8 @@ function App() {
             <Route path="/dashboard" element={<Dashboard />} />
           </Route>
         </Routes>
-        <WhatsAppButton />
         <BackToTop />
+        <WhatsAppButton />
         <ThemeToggle />
       </div>
     </Router>
